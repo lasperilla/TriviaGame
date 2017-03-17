e65ef8249a528b26d0ebb172f0ef05b0bade5402
lasperilla <l.asperilla@gmail.com>
2017-03-17 01:48:01 -0400

    added reset function, favicon.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@ window.onload = function() {
     $('.my-start-btn').click(start);
 };
 
-var timeLeft = 10;
+var timeGiven = 10;
 var userAnswersArr = [];
 var correctAnswers = 0;
 var incorrectAnswers = 0;
@@ -91,10 +91,13 @@ for (var i = 0; i < triviaArr.length; i++) {
 };
 
 
+
 function start() {
     $('.my-start-btn').css({ 'display': 'none' });
     $('.trivia-body').css({ 'display': 'block' });
-    $('.trivia-body h1').html('<h1>Time Remaining: ' + timeLeft + ' Seconds</h1>');
+    var timeLeft=timeGiven;
+    $('.trivia-body h1').html('<h1>Time Remaining: ' + timeGiven + ' Seconds</h1>');
+    
 
     //timer
     function decrement() {
@@ -103,39 +106,64 @@ function start() {
         var printMe = 'Time Remaining: ' + timeLeft + ' Seconds';
         $('.trivia-body h1').html(printMe);
         if (timeLeft === 0) {
-        	$('.trivia-body h1').css({ 'display': 'none' })
-            clearInterval(timer);
-            	// alert('time');
-
-			for (var i = 0; i < triviaArr.length; i++) {
-
-				var groupName = 'question'+eval(i+1);
-				var result = $('input[data-group="'+groupName+'"]:checked').val();
-				userAnswersArr.push(result);
-				console.log(userAnswersArr);
-			}
-
-			for (var i = 0; i < triviaArr.length; i++) {
-				if (triviaArr[i].answer === userAnswersArr[i]) {
-					correctAnswers++;
-				} else if (userAnswersArr[i] === undefined) {
-					unanswered++;
-				} else {
-					incorrectAnswers++;
-				}
-			}
-
-			$('.trivia-body').html("<h1>Results:<h1>"+ "<p>Correct Answers: "+correctAnswers+"</p>"+"<p>Incorrect Answers: "+incorrectAnswers+"</p>"+"<p>Unanswered Questions: "+unanswered+"</p>")
-			//alternate results if you get all right or if you get all wrong
-			if (correctAnswers === triviaArr.length) {
-				$('.trivia-body').html('<div><iframe width="374" height="210" src="https://www.youtube.com/embed/oe2Gcuu8mEE?autoplay=1" frameborder="0" allowfullscreen></iframe></div>'+"<h1>Great Job! Results:<h1>"+ "<p class=\"correct\">Correct Answers: "+correctAnswers+"</p>"+"<p>Incorrect Answers: "+incorrectAnswers+"</p>"+"<p>Unanswered Questions: "+unanswered+"</p>")
-			} else if (incorrectAnswers === triviaArr.length) {
-				$('.trivia-body').html('<div><iframe width="374" height="210" src="https://www.youtube.com/embed/O4buGpzS-2k?autoplay=1" frameborder="0" allowfullscreen></iframe></div>'+"<h1>Get some sleep and try again tomorrow. Results:<h1>"+ "<p>Correct Answers: "+correctAnswers+"</p>"+"<p class=\"incorrect\">Incorrect Answers: "+incorrectAnswers+"</p>"+"<p>Unanswered Questions: "+unanswered+"</p>")
-			}
+        	resultsFunc();
         }
     }
 
     var timer = setInterval(decrement, 1000);
+
+    //win loss evaluation function
+	function resultsFunc() {
+
+	        	$('.trivia-body h1').css({ 'display': 'none' })
+	            clearInterval(timer);
+	            	// alert('time');
+
+				for (var i = 0; i < triviaArr.length; i++) {
+
+					var groupName = 'question'+eval(i+1);
+					var result = $('input[data-group="'+groupName+'"]:checked').val();
+					userAnswersArr.push(result);
+					console.log(userAnswersArr);
+				}
+
+				for (var i = 0; i < triviaArr.length; i++) {
+					if (triviaArr[i].answer === userAnswersArr[i]) {
+						correctAnswers++;
+					} else if (userAnswersArr[i] === undefined) {
+						unanswered++;
+					} else {
+						incorrectAnswers++;
+					}
+				}
+
+				$('.trivia-body').html("<h1>Results:<h1>"+ "<p>Correct Answers: "+correctAnswers+"</p>"+"<p>Incorrect Answers: "+incorrectAnswers+"</p>"+"<p>Unanswered Questions: "+unanswered+"</p><button class=\"btn-default reset-btn\">Play Again</button>")
+				//alternate results if you get all right or if you get all wrong
+				if (correctAnswers === triviaArr.length) {
+					$('.trivia-body').html('<div><iframe width="374" height="210" src="https://www.youtube.com/embed/oe2Gcuu8mEE?autoplay=1" frameborder="0" allowfullscreen></iframe></div>'+"<h1>Great Job! Results:<h1>"+ "<p class=\"correct\">Correct Answers: "+correctAnswers+"</p>"+"<p>Incorrect Answers: "+incorrectAnswers+"</p>"+"<p>Unanswered Questions: "+unanswered+"</p><button class=\"btn-default reset-btn\">Play Again</button>")
+				} else if (incorrectAnswers === triviaArr.length) {
+					$('.trivia-body').html('<div><iframe width="374" height="210" src="https://www.youtube.com/embed/O4buGpzS-2k?autoplay=1" frameborder="0" allowfullscreen></iframe></div>'+"<h1>Get some sleep and try again tomorrow. Results:<h1>"+ "<p>Correct Answers: "+correctAnswers+"</p>"+"<p class=\"incorrect\">Incorrect Answers: "+incorrectAnswers+"</p>"+"<p>Unanswered Questions: "+unanswered+"</p><button class=\"btn-default reset-btn\">Play Again</button>")
+				}
+
+				$('.reset-btn').on('click', function(){
+					console.log('reset');
+				    $('.my-start-btn').css({ 'display': 'block' });
+				    $('.trivia-body').css({ 'display': 'none' });
+				    $('.trivia-body').html('<h1>Time Remaining:</h1><div class="questions"></div><button class="btn-default done-btn">Done</button>')
+				    userAnswersArr = [];
+					correctAnswers = 0;
+					incorrectAnswers = 0;
+					unanswered = 0;
+				});
+	}
+
+	$('.done-btn').click(function(){
+		timeLeft=0;
+		resultsFunc();
+	});
+
+
+
 
     //for length of our array containing our trivia, creat i number of sets of questions and answers, format them the way we want them displayed on the page, then push them in our questions div
 
@@ -155,5 +183,7 @@ function start() {
     };
 
 };//end of start()
+
+
 
 
